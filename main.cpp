7300211737c7f0cdc24d0831cb98b31792972ba9
yang <capitanyang@gmail.com>
2012-03-29 14:32:37 +0800

    fix some bugs, there are still bugs

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "redblacktree.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "i am yang" << endl;
+    redblacktree<int> test;
+    int i(0);
+    for(; i < 10; ++i)
+    {
+        test.RB_insert(i);
+    }
+    test.pre_tree_walk();
     return 0;
 }
