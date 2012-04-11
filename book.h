fcfd7b8c3f78bcfb2f4c78023721c81c26f0bd19
lxyangzw <1521805807@qq.com>
2012-04-11 08:54:05 +0800

    add book and user

---
+++ book.h
@@ -0,0 +1,99 @@
+/*图书类的实现
+ * by ya
+ */
+#ifndef BOOK_H_INCLUDED
+#define BOOK_H_INCLUDED
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+enum State{clean=0, borrowed, advanced-borrowed};
+
+class book
+{
+priate:
+	string bkname;
+	string author;
+	string isbn;
+	State state;
+	public:
+	book(){ state = clean;}
+	book(const string& name);
+	book(const string& name, const string& ibn, const string& ath)
+	book(const book& bk);//复制构造函数
+	operator=(const book& bk);//赋值操作符
+	void getinfor();
+	string getbkname() const;
+	string getauthor() const;
+	string getisbn() const;
+	void getstate();
+	void setstate(const State& state);
+	void setbkname(const string& name);
+	void setauthor(const string& author);
+	void setisbn(const string& isbn);
+};
+
+book::book(const string& name):bkname(name),state(clean){};
+
+book::book(const string& name, const string& ibn, const string& ath):bkname(name),isbn(ibn),author(ath),state(clean){};
+
+book::book(const book& bk):bkname(bk.bkname),isbn(bk.isbn),author(bk.author),state(bk.state){};
+
+book::operator=(const book& bk)
+{
+	bkname = bk.bkname;
+	isbn = bk.isbn;
+	author = bk.author;
+	state = bk.state;
+	return *this;
+}
+
+void book::getinfor()
+{
+	cout << "Bookname: " << bkname << endl;
+	cout << "Author: " << author << endl;
+	cout << "Isbn: " << isbn << endl;
+	cout << "State: " << state << endl;
+}
+
+string book::getbkname() const
+{
+	return bkname;
+}
+
+string book::getauthor() const
+{
+	return author;
+}
+
+string book::getisbn() const
+{
+	return isbn;
+}
+
+void book::getstate()
+{
+	cout << "State: " << state << endl;
+}
+
+void book::setstate(const State& state)
+{
+	this->state = state;
+}
+
+void book::setbkname(const string& bkname)
+{
+	this->bkname = bkname;
+}
+
+void book::setauthor(const string& author)
+{
+	this->author = author;
+}
+
+void book::setisbn(const string& isbn)
+{
+	this->isbn = isbn;
+}
+#endif // USER_H_INCLUDED
--- user.h
+++ user.h
@@ -1,6 +1,154 @@
+/*用户类的实现
+ * by ya
+ */
 #ifndef USER_H_INCLUDED
 #define USER_H_INCLUDED
+#include<string>
+#include"book.h"
+#include<iostream>
+#define BORROWBOOKLIMIT 12
 
+using namespace std;
 
+class user
+{
+	private:
+		string name;//姓名
+		string number;//学号
+		string email;//邮箱
+		book* mybook[BORROWBOOKLIMIT];//借的书
+		int booknum;//借书的数量
+		bool isbookfull() const;//判断是否已经借满了书
+	public:
+		user();
+		user(const string& name);
+		user(const string& name, const string& number, const string& email);
+		user(const user &usr);//复制构造函数
+		operator=(const user &usr);//赋值操作符
+		void getinfor();
+		string getname() const;
+		string getnumber() const;
+		string getemail() const;
+		void getbookinfor();
+		void setname(const string& name);
+		void setnumber(const string& number);
+		void setemail(const string& email);
+		bool addbook(book* newbook);//用户借书
+		int getbooknumb() const;
+		void returnbook(book* rbook);//用户还书
+};
 
+user::user()
+{
+	for(int i = 0; i < BORROWBOOKLIMIT; ++i)
+		mybook[i] = NULL;
+}
+
+user::user(const string& name)
+{
+	this->name = name;
+	for(int i = 0; i < BORROWBOOKLIMIT; ++i)
+		mybook[i] = NULL;
+}
+
+user::user(const string& name, const string* number, const tring& email)
+{
+	this->name = name;
+	this->number = number;
+	this->email = email;
+	for(int i = 0; i < BORROWBOOKLIMIT; ++i)
+		mybook[i] = NULL;
+}
+
+//复制构造函数
+user::user(const user &usr)
+{
+	name = usr.name;
+	number = usr.number;
+	email = usr.email;
+	for(int i = 0; i < BORROWBOOKLIMIT; ++i)
+		mybook[i] = usr.mybook[i];
+}
+
+user::operator=(const user &usr)
+{
+	name = usr.name;
+	number = usr.number;
+	email = usr.email;
+	for(int i = 0; i < BORROWBOOKLIMIT; ++i)
+		mybook[i] = usr.mybook[i];
+	return *this;
+}
+
+bool user::isbookfull() const
+{
+	return booknum == BORROWBOOKLIMIT;
+}
+
+void user::getinfo()
+{
+	cout << "Name: " << endl;
+	cout << "Xuehao: " << endl;
+	cout << "Email: " << endl;
+	cout << "Borrowed BookNumber: " << endl;
+}
+
+string user::getname() const
+{
+	return name;
+}
+
+string user::getnumber() const
+{
+	return number;
+}
+
+string user::getemail() const
+{
+	return email;
+}
+
+void user::getbookinfo()
+{
+
+}
+
+void setname(const string& name)
+{
+	 this->name = name;
+}
+
+void  setnumber(const string& number)
+{
+	this->number = number;
+}
+
+void setemail(const string& email)
+{
+	this->email = email;
+}
+
+bool addbook(book* newbook)
+{
+	if(ibookful())
+		return false;
+	else
+	{
+		mybook*[booknum++] = newbook;
+		return true;
+	}
+}
+
+int getbooknumb() const
+{
+	return booknum;
+}
+
+void returnbook(book* rbook)
+{
+	int i(0);
+	for(; i < BORROWBOOKLIMIT, mybook[i] != rbook; ++i);
+	mybook[i] = NULL;
+	booknum --;
+}
 #endif // USER_H_INCLUDED
